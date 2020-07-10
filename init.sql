14285a2e107e698c48f3647318e9dd4aece88e76
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-10 14:07:02 +0300

    Added SQL script for creating the structure of db

---
+++ init.sql
@@ -0,0 +1,44 @@
+create database if not exists travel_agency;
+
+use travel_agency;
+
+create table if not exists `hotels` (
+    `id` int primary key auto_increment,
+    `name` varchar(100) not null,
+    `description` varchar(200) not null,
+    `country` varchar(30) not null
+);
+
+create table if not exists `rooms` (
+  `id` int primary key auto_increment,
+  `room_number` int not null,
+  `hotel_id` bigint not null,
+   foreign key (hotel_id)
+      references hotels(`id`)
+        on update restrict on delete cascade
+);
+
+create table if not exists `users` (
+  `id` int primary key  auto_increment,
+  `email` varchar(100) not null unique,
+  `name` varchar(200) not null,
+  `password` varchar(200) not null
+);
+
+create table if not exists `roles` (
+  `id` int primary key auto_increment,
+  `name` varchar(50) not null unique
+);
+
+create table if not exists `user_roles` (
+  `id` int primary key auto_increment,
+  `user_id` int not null,
+  `role_id` int not null,
+        foreign key (user_id)
+            references users(`id`)
+            on update restrict on delete cascade,
+        foreign key (role_id)
+            references roles(`id`)
+            on update restrict on delete cascade
+);
+
