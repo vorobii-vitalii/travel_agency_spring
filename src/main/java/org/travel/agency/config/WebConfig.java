85e71c11ce78a812c112ed7559a0d2f0cbe6b612
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-10 13:45:57 +0300

    Added WebConfig

---
+++ src/main/java/org/travel/agency/config/WebConfig.java
@@ -0,0 +1,63 @@
+package org.travel.agency.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.travel.agency")
+@AllArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final ApplicationContext applicationContext;
+
+    /**
+     * The method sets up view resolver for the application
+     * @see ViewResolver
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/templates/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setApplicationContext(applicationContext);
+        return viewResolver;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(viewResolver());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        setStaticResourceHandling(registry);
+        setImageResourceHandling(registry);
+    }
+
+    private void setStaticResourceHandling(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+    }
+
+    private void setImageResourceHandling(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/WEB-INF/images/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+    }
+
+}
