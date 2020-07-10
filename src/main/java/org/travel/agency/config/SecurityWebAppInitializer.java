f3f869df38e4d47acabbf62b6984bb4521adf871
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-10 13:54:32 +0300

    Added SecurityWebAppInitializer

---
+++ src/main/java/org/travel/agency/config/SecurityWebAppInitializer.java
@@ -0,0 +1,6 @@
+package org.travel.agency.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
