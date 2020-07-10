deef47c687120aff7318f267ae47687319fc293f
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-10 13:54:13 +0300

    Added DispatcherServletInitializer

---
+++ src/main/java/org/travel/agency/config/DispatcherServletInitializer.java
@@ -0,0 +1,37 @@
+package org.travel.agency.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class, HibernateConfig.class, SecurityConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+    /**
+     * Forces UTF-8 encoding in an application
+     */
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[] {characterEncodingFilter};
+    }
+
+
+}
