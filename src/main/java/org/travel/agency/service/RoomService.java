5231774dd4ae9c179afbd6042eb80322d4635327
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-14 13:29:36 +0300

    Added RoomService

--- src/main/java/org/travel/agency/service/RoomService.java
+++ src/main/java/org/travel/agency/service/RoomService.java
@@ -2,10 +2,13 @@ package org.travel.agency.service;
 
 import org.travel.agency.entity.Room;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RoomService {
     List<Room> getAllByHotelId(Long hotelId);
+    List<Room> getAllByHotelIdAndDateBeyond(Long hotelId, LocalDate fromDate, LocalDate tillDate);
+    boolean havingOrdersDuringDateBetween(Long roomId, LocalDate dateFrom, LocalDate dateTill);
     Room getById(Long id);
     void save(Room roomToSave);
     Room deleteById(Long id);
