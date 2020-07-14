ad7f6f5bdb8df6f2cda318eda1e60f43f9d2fee3
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-14 13:17:45 +0300

    Updated RoomDAO

--- src/main/java/org/travel/agency/dao/RoomDAO.java
+++ src/main/java/org/travel/agency/dao/RoomDAO.java
@@ -2,12 +2,15 @@ package org.travel.agency.dao;
 
 import org.travel.agency.entity.Room;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface RoomDAO {
     List<Room> findAllByHotelId(Long hotelId);
     Optional<Room> findById(Long id);
+    List<Room> findAllByHotelIdAndDateBetween(Long hotelId, LocalDate dateFrom, LocalDate dateTill);
+    boolean havingOrdersDuringDateBetween(Long roomId, LocalDate dateFrom, LocalDate dateTill);
     void save(Room room);
     void deleteById(Long id);
 }
