75e92f45996baaddfd3713cd40c2cb092989ad44
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-14 14:08:31 +0300

    Added HotelController

--- src/main/java/org/travel/agency/controller/HotelController.java
+++ src/main/java/org/travel/agency/controller/HotelController.java
@@ -2,10 +2,16 @@ package org.travel.agency.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.travel.agency.entity.Country;
 import org.travel.agency.entity.Hotel;
+import org.travel.agency.service.CountryService;
 import org.travel.agency.service.HotelService;
+import org.travel.agency.validation.HotelValidator;
+
+import java.util.List;
 
 
 @Controller
@@ -13,6 +19,8 @@ import org.travel.agency.service.HotelService;
 @AllArgsConstructor
 public class HotelController {
     private final HotelService hotelService;
+    private final CountryService countryService;
+    private final HotelValidator hotelValidator;
 
     @GetMapping
     public ModelAndView getAllHotels() {
@@ -25,11 +33,29 @@ public class HotelController {
     public ModelAndView showAddForm() {
         ModelAndView  modelAndView = new ModelAndView("add_hotel");
         modelAndView.addObject("hotel", new Hotel());
+        List<Country> countryList = countryService.getAll();
+        System.out.println(countryList);
+        modelAndView.addObject("countries", countryList);
         return modelAndView;
     }
 
-    @PostMapping("/save")
-    public String saveHotel(@ModelAttribute("hotel") Hotel hotel) {
+    @PostMapping("/edit")
+    public String editTheHotel(@ModelAttribute("hotel") Hotel hotel, Errors errors) {
+        System.out.println(hotel);
+        hotelValidator.validate(hotel, errors);
+        if (errors.hasErrors()) {
+            return "edit_hotel";
+        }
+        hotelService.save(hotel);
+        return "redirect:/hotels";
+    }
+
+    @PostMapping("/add")
+    public String addNewHotel(@ModelAttribute("hotel") Hotel hotel, Errors errors) {
+        hotelValidator.validate(hotel, errors);
+        if (errors.hasErrors()) {
+            return "add_hotel";
+        }
         hotelService.save(hotel);
         return "redirect:/hotels";
     }
@@ -38,6 +64,7 @@ public class HotelController {
     public ModelAndView editHotelById(@PathVariable Long id) {
         ModelAndView modelAndView = new ModelAndView("edit_hotel");
         modelAndView.addObject("hotel", hotelService.getById(id));
+        modelAndView.addObject("countries", countryService.getAll());
         return modelAndView;
     }
 
