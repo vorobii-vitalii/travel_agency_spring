1f1cbecbd6793ed9697e1a746078fbcacc395925
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-14 16:06:21 +0300

    Added data.sql

---
+++ data.sql
@@ -0,0 +1,17 @@
+use travel_agency;
+
+-- Add roles
+
+insert into roles (name) values ('CUSTOMER');
+insert into roles (name) values ('MANAGER');
+
+-- Add some countries
+
+insert into countries (name) values ('USA');
+insert into countries (name) values ('Ukraine');
+insert into countries (name) values ('Sweden');
+insert into countries (name) values ('Canada');
+insert into countries (name) values ('Italy');
+insert into countries (name) values ('Spain');
+insert into countries (name) values ('Australia');
+
