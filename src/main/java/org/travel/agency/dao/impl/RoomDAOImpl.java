844959e546d799aa5a0c08ca6940ad46ff4633db
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-14 13:19:40 +0300

    Updated RoomDAOImpl

--- src/main/java/org/travel/agency/dao/impl/RoomDAOImpl.java
+++ src/main/java/org/travel/agency/dao/impl/RoomDAOImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import org.travel.agency.dao.RoomDAO;
 import org.travel.agency.entity.Room;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,7 +20,7 @@ public class RoomDAOImpl implements RoomDAO {
     public List<Room> findAllByHotelId(Long hotelId) {
         Session session = sessionFactory.getCurrentSession();
         return session
-                .createQuery("select r from Room r inner join Hotel h on h.id = :hotelId", Room.class)
+                .createQuery("select r from Room r where r.hotelId=:hotelId", Room.class)
                 .setParameter("hotelId", hotelId )
                 .list();
     }
@@ -31,6 +32,35 @@ public class RoomDAOImpl implements RoomDAO {
                 .createQuery("select r from Room r where r.id=:id", Room.class)
                 .setParameter("id", id)
                 .uniqueResultOptional();
+    }
+
+    @Override
+    public List<Room> findAllByHotelIdAndDateBetween(Long hotelId, LocalDate dateFrom, LocalDate dateTill) {
+        Session session = sessionFactory.getCurrentSession();
+        return session
+                .createQuery(
+                "select r from Room r " +
+                    "where r.hotelId=:hotelId and " +
+                    "not exists (select o from Order o where o.room = r " +
+                        "and ( o.dateFrom <= :dateTill and o.dateTill > :dateFrom  )" +
+                    ")", Room.class)
+                .setParameter("hotelId", hotelId)
+                .setParameter("dateFrom", dateFrom)
+                .setParameter("dateTill", dateTill)
+                .list();
+    }
+
+    public boolean havingOrdersDuringDateBetween(Long roomId, LocalDate dateFrom, LocalDate dateTill) {
+        Session session = sessionFactory.getCurrentSession();
+        return session
+                .createQuery("select r from Room r where r.id = :roomId " +
+                        "and exists(select o from Order o where o.room.id = r.id " +
+                        "and (o.dateFrom <= :dateTill and o.dateTill > :dateFrom )" +
+                        ") ")
+                .setParameter("roomId", roomId)
+                .setParameter("dateFrom", dateFrom)
+                .setParameter("dateTill", dateTill)
+                .uniqueResult() != null;
     }
 
     @Override
