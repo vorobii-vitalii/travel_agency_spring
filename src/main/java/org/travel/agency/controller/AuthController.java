4d9f3639bd315ebfd89ea7511e4ec51cac5bcfaf
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-14 14:06:15 +0300

    Added AuthController

---
+++ src/main/java/org/travel/agency/controller/AuthController.java
@@ -0,0 +1,55 @@
+package org.travel.agency.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.travel.agency.entity.User;
+import org.travel.agency.security.SecurityService;
+import org.travel.agency.service.AuthService;
+import org.travel.agency.validation.UserValidator;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping
+public class AuthController {
+    private final AuthService authService;
+    private final SecurityService securityService;
+    private final UserValidator userValidator;
+
+    @GetMapping("/login")
+    public ModelAndView showLoginForm(String error, String logout) {
+        ModelAndView modelAndView = new ModelAndView("login");
+        if (error != null) {
+            modelAndView.addObject("error", "Credentials are incorrect");
+        }
+        if (logout != null) {
+            modelAndView.addObject("message", "You have successfully logged out");
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/register")
+    public ModelAndView showRegistrationForm() {
+        ModelAndView modelAndView = new ModelAndView("registration");
+        modelAndView.addObject("userToRegister", new User());
+        return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public String processRegistration(@ModelAttribute("userToRegister") User user, Errors errors) {
+        userValidator.validate(user, errors);
+        if (errors.hasErrors()) {
+            return "registration";
+        }
+        String email = user.getEmail(), password = user.getPassword();
+        authService.registerUser(user);
+        securityService.autoLogin(email, password);
+        return "redirect:/hotels";
+    }
+
+}
