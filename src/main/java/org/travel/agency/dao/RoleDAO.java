a3f46ffebcfde44c64bd1d5838cd4a560f2beb6c
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-14 13:16:37 +0300

    Added RoleDAO

---
+++ src/main/java/org/travel/agency/dao/RoleDAO.java
@@ -0,0 +1,11 @@
+package org.travel.agency.dao;
+
+import org.travel.agency.entity.Role;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RoleDAO {
+    List<Role> findAll();
+    Optional<Role> findByName(String name);
+}
