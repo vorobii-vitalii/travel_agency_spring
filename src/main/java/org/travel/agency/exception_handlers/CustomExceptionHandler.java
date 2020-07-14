46557fb9a2a3eae007b17e8863d32b6b4fb4365b
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-14 13:06:59 +0300

    Updated CustomExceptionHandler

--- src/main/java/org/travel/agency/exception_handlers/CustomExceptionHandler.java
+++ src/main/java/org/travel/agency/exception_handlers/CustomExceptionHandler.java
@@ -1,14 +1,24 @@
 package org.travel.agency.exception_handlers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.travel.agency.exceptions.NotFoundException;
 
 @ControllerAdvice
+@Slf4j
 public class CustomExceptionHandler {
 
     @ExceptionHandler(value = NotFoundException.class)
     public String handleNotFoundException() {
+        log.error("404 error occurred");
+        return "not_found";
+    }
+
+    @ExceptionHandler(value = NoHandlerFoundException.class)
+    public String handleNotAvailableEndpoint() {
+        log.error("Request to not available endpoint");
         return "not_found";
     }
 
