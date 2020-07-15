66ff776e28a78034abb2e05c45136c13126aaf50
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-07-15 12:48:25 +0300

    Added AuthControllerTest

---
+++ src/test/java/org/travel/agency/controller/AuthControllerTest.java
@@ -0,0 +1,48 @@
+package org.travel.agency.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.travel.agency.config.HibernateConfig;
+import org.travel.agency.config.WebConfig;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        classes = { HibernateConfig.class, WebConfig.class},
+        loader = AnnotationConfigWebContextLoader.class
+)
+@Transactional(propagation= Propagation.REQUIRED)
+@WebAppConfiguration
+public class AuthControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AuthController authController;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
+    }
+
+    @Test
+    public void testRegisterViewName() throws Exception {
+        mockMvc.perform(get("/register"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("registration"));
+    }
+
+}
